c9192e9ef553e72579bc23bafeb173aeac3e761e
Christian Câmara de Abreu <christian@MacBook-Air-de-Christian.local>
2021-03-13 22:35:08 -0300

    setando numeros na calculadora

--- App.js
+++ App.js
@@ -27,17 +27,38 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+const initialState = {
+  displayValue : '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0,0],
+  current: 0,
+}
+
 export default class App extends Component {
-  state ={
-    displayValue:'0'
-  }
+  state ={...initialState}
 
   addDigit = n =>{
-    this.setState({displayValue: n})
+    if (n ==='.' && this.state.displayValue.includes('.')){
+      return
+    }
+    if(this.state.displayValue.length == 11){
+      return
+    }
+    const clearDisplay=this.state.displayValue==='0'||this.state.clearDisplay;
+    const currentValue = clearDisplay?'':this.state.displayValue;
+    const displayValue=currentValue+n
+    this.setState({displayValue,clearDisplay:false})
+    if(n!=='.'){
+      const newValue=parseFloat(displayValue)
+      const values=[...this.state.values]
+      values[this.state.current]=newValue
+      this.setState({values})
+    }
   }
 
   clearMemory = () => {
-    this.setState({displayValue:0})
+    this.setState({...initialState})
   }
 
   setOperation = () => {
@@ -50,22 +71,22 @@ export default class App extends Component {
         <Display value={this.state.displayValue}></Display>
         <View style={styles.buttons}>
           <Button label="AC" triple onClick={this.clearMemory} />
-          <Button label="/" operation onClick={this.addDigit} />
+          <Button label="/" operation onClick={this.setOperation} />
           <Button label="7"  onClick={this.addDigit} />
           <Button label="8"  onClick={this.addDigit}/>
           <Button label="9"  onClick={this.addDigit}/>
-          <Button label="*" operation   onClick={this.addDigit}/>
+          <Button label="*" operation   onClick={this.setOperation}/>
           <Button label="4"  onClick={this.addDigit}/>
           <Button label="5"  onClick={this.addDigit}/>
           <Button label="6"  onClick={this.addDigit}/>
-          <Button label="-" operation  onClick={this.addDigit}/>
+          <Button label="-" operation  onClick={this.setOperation}/>
           <Button label="1"  onClick={this.addDigit}/>
           <Button label="2"  onClick={this.addDigit}/>
           <Button label="3"  onClick={this.addDigit}/>
-          <Button label="+" operation  onClick={this.addDigit}/>
+          <Button label="+" operation  onClick={this.setOperation}/>
           <Button label="0"   onClick={this.addDigit}/>
           <Button label="."  onClick={this.addDigit}/>
-          <Button label="=" operation  onClick={this.addDigit}/>
+          <Button label="=" operation  onClick={this.setOperation}/>
         </View>
       </SafeAreaView>
     );
